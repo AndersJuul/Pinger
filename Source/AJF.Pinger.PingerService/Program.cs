d8738b152c6f5b7de036b88ba8f4de34b1def43e
Anders Juul <aju@vertica.dk>
2017-04-20 11:48:23 +0200

    a

--- Source/AJF.Pinger.PingerService/Program.cs
+++ Source/AJF.Pinger.PingerService/Program.cs
@@ -1,15 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using Topshelf;
 
 namespace AJF.Pinger.PingerService
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                //.WriteTo.RollingFile(ConfigurationManager.AppSettings["RollingFile"])
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost.:9200")))
+                .CreateLogger();
+
+            try
+            {
+                HostFactory.Run(x => //1
+                {
+                    x.Service<Worker>(s => //2
+                    {
+                        try
+                        {
+                            s.ConstructUsing(name => new Worker()); //3
+                            s.WhenStarted(tc =>
+                            {
+                                Log.Logger.Information("Starting service.");
+                                tc.Start();
+                                Log.Logger.Information("Service started.");
+                            }); //4
+                            s.WhenStopped(tc =>
+                            {
+                                Log.Logger.Information("Stopping service.");
+                                tc.Stop();
+                                Log.Logger.Information("Service stopped.");
+                            }); //5
+                            s.WhenPaused(tc =>
+                            {
+                                Log.Logger.Information("Pausing service.");
+                                tc.Stop();
+                                Log.Logger.Information("Service paused.");
+                            }); //5
+
+                            s.WhenContinued(tc =>
+                            {
+                                Log.Logger.Information("Continuing service.");
+                                tc.Start();
+                                Log.Logger.Information("Service continued.");
+                            }); //5
+                            s.WhenSessionChanged((w, sca) =>
+                            {
+                                Log.Logger.Information("Session changed: " + w);
+                                Log.Logger.Information("Session changed: " + sca);
+                            });
+
+
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex, "Fra program");
+                            throw;
+                        }
+                    });
+                    x.RunAsLocalSystem(); //6
+
+                    x.SetDescription("Flytter fotos fra feks dropbox til NAS"); //7
+                    x.SetDisplayName("AJF.PhotoMover"); //8
+                    x.SetServiceName("AJF.PhotoMover"); //9
+                }); //10        }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Yderste");
+                throw;
+            }
         }
     }
 }
---
+++ Source/AJF.Pinger.PingerService/Worker.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using Serilog;
+
+namespace AJF.Pinger.PingerService
+{
+    public class Worker
+    {
+        private const string ConfigFileName = "photomover.config";
+        private BackgroundWorker _backgroundWorker;
+        //private List<IPhotoMoverConfig> _config;
+        public bool WorkDone { get; set; }
+
+        public void Start()
+        {
+            try
+            {
+                //var pathList = ConfigurationManager.AppSettings["PathList"].Split(';');
+                //_config = new List<IPhotoMoverConfig>();
+
+                //foreach (var path in pathList)
+                //{
+                //    Log.Logger.Information("Watching: " + path);
+                //    var photoMoverConfig = new PhotoMoverConfig
+                //    {
+                //        Path = path
+                //    };
+                //    _config.Add(photoMoverConfig);
+                //}
+
+                _backgroundWorker = new BackgroundWorker
+                {
+                    WorkerSupportsCancellation = true
+                };
+                _backgroundWorker.DoWork += _backgroundWorker_DoWork;
+                _backgroundWorker.RunWorkerCompleted += _backgroundWorker_RunWorkerCompleted;
+                _backgroundWorker.RunWorkerAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "During Start", new object[0]);
+                throw;
+            }
+        }
+
+        private void _backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            WorkDone = true;
+        }
+
+        private void _backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Log.Logger.Information("Doing work");
+
+            try
+            {
+                DoWorkInternal(sender);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "During do work", new object[0]);
+                throw;
+            }
+        }
+
+        private void DoWorkInternal(object sender)
+        {
+            WorkDone = false;
+
+            while (true)
+            {
+                var backgroundWorker = sender as BackgroundWorker;
+                if (backgroundWorker == null || backgroundWorker.CancellationPending)
+                {
+                    Log.Information("backgroundworker.CancellationPending: {@backgroundWorker}", backgroundWorker);
+                    return;
+                }
+
+                Thread.Sleep(1 * 500);
+                Log.Information("Hello world");
+
+            }
+        }
+
+
+        public void Stop()
+        {
+            if (_backgroundWorker != null)
+            {
+                _backgroundWorker.CancelAsync();
+
+                while (!WorkDone)
+                    Thread.Sleep(500);
+            }
+        }
+    }
+}
